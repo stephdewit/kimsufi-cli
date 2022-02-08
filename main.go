d3d99cece9e3fb7701ce98aecd0dc560552522b7
Stéphane de Wit <contact@stephanedewit.be>
2022-02-08 22:55:02 +0100

    Replace hardcoded values with parameters

--- main.go
+++ main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/stephdewit/kimsufi-cli/availability"
 	"os"
 )
 
 func main() {
-	availability, err := availability.Get("fr", "europe", "2201sk011")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Can't get availability: %v\n", err)
+	availabilityCommand := flag.NewFlagSet("availability", flag.ExitOnError)
+	availabilityCountry := availabilityCommand.String("country", "fr", "Country")
+	availabilityRegion := availabilityCommand.String("region", "europe", "Region")
+
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Missing sub-command\n")
 		os.Exit(1)
 	}
 
-	fmt.Println(availability)
+	switch os.Args[1] {
+	case "availability":
+		availabilityCommand.Parse(os.Args[2:])
+
+		if len(availabilityCommand.Args()) < 1 {
+			fmt.Fprintf(os.Stderr, "Missing hardware parameter\n")
+			os.Exit(1)
+		}
+
+		hardware := availabilityCommand.Args()[0]
+
+		if hardware == "" {
+			fmt.Fprintf(os.Stderr, "Empty hardware parameter\n")
+		}
+
+		availability, err := availability.Get(
+			*availabilityCountry,
+			*availabilityRegion,
+			hardware)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(availability)
+	default:
+		fmt.Fprintf(os.Stderr, "Expected 'availability' sub-command\n")
+		os.Exit(1)
+	}
 }
